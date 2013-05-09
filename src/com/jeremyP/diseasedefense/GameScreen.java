f015b2957ce441fef17b43b03373e8a25c93d9bd
javaknight1 <robertaveryiv@gmail.com>
2013-05-09 15:45:18 -0400

    Enemy, when created and killed, have all there health.

--- src/com/jeremyP/diseasedefense/GameScreen.java
+++ src/com/jeremyP/diseasedefense/GameScreen.java
@@ -121,6 +121,7 @@ public class GameScreen extends Screen {
 		{
 			System.out.println(enemy.get(enemyindex).getCurrentHealth());
 			enemy.get(enemyindex).getHit();
+			enemy.get(enemyindex).revive();
 			System.out.println("Hit: " + enemy.get(enemyindex).getCurrentHealth());
 			character.stopProjectile();
 			if (enemy.get(enemyindex).isDead()) 
@@ -136,6 +137,7 @@ public class GameScreen extends Screen {
 				{
 					state = GameState.Beaten;
 				}
+				
 				//You've gone to the next level
 				else if(level.isChanged()){
 					int enemySum = enemy.size();
@@ -148,7 +150,14 @@ public class GameScreen extends Screen {
 					int points = enemy.get(enemySum-1).getSpeed() +1;
 					
 					//TODO: Add more new enemy types into the assets
+					
+					//Add old enemies from enemy array starting brand new
+					/*for(int i=0; i < enemy.size(); i++){
+						enemy.get(i).revive();
+					}*/
+					
 					//This if statement will stay here until more enemy variations are created
+					//TODO: Change the 3 as more enemy pictures are made
 					if(level.whatLevel() < 3){
 						Enemy newenemy = new Enemy(g, Assets.badGuys[level.whatLevel()-1] ,speed, health, points);
 						
