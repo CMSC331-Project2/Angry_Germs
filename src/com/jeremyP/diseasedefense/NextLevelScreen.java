84c4cbb7a9a8727dca4a6402ba077b23a22e8745
javaknight1 <robertaveryiv@gmail.com>
2013-05-09 15:12:39 -0400

    Added NextLevelScreen

---
+++ src/com/jeremyP/diseasedefense/NextLevelScreen.java
@@ -0,0 +1,21 @@
+package com.jeremyP.diseasedefense;
+
+import com.jeremyP.diseasedefense.framework.Game;
+import com.jeremyP.diseasedefense.framework.Graphics;
+import com.jeremyP.diseasedefense.framework.Screen;
+
+public class NextLevelScreen extends Screen {
+
+	private Graphics g;
+	
+	public NextLevelScreen(Game game, Level level){
+		super(game);
+		
+		
+	}
+	
+	
+	
+	
+	
+}
