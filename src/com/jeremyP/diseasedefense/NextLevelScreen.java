4aab9cc2b353936a3486e4de03d708af8aa71fc6
javaknight1 <robertaveryiv@gmail.com>
2013-05-09 15:14:39 -0400

    Working NextLevelScreen

--- src/com/jeremyP/diseasedefense/NextLevelScreen.java
+++ src/com/jeremyP/diseasedefense/NextLevelScreen.java
@@ -13,9 +13,36 @@ public class NextLevelScreen extends Screen {
 		
 		
 	}
-	
-	
-	
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void present(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
